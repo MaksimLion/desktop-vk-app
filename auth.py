6179e891bbed6084af05e9525c7963209e763c0b
MaksimLion <maxim226356@mail.ru>
2019-04-15 17:02:42 +0300

    implemented two factor authentication

--- auth.py
+++ auth.py
@@ -1,11 +1,11 @@
 import sys
-import vk_tools
 
+from vk_tools import VkTools
 from design import auth_design
 from PyQt5 import QtCore, QtGui, QtWidgets
 
     
-class Auth(QtWidgets.QMainWindow, auth_design.Ui_MainWindow):
+class Auth(QtWidgets.QMainWindow, auth_design.Ui_MainWindow, VkTools):
 
     def __init__(self):
         super().__init__()
@@ -13,22 +13,23 @@ class Auth(QtWidgets.QMainWindow, auth_design.Ui_MainWindow):
         self.password_input.setEchoMode(QtWidgets.QLineEdit.Password)
         self.auth_btn.clicked.connect(self.authentication)
 
-    def show_input_key_dialog(self):
-        self.secret_key, ok = QtWidgets.QInputDialog.getText(self, 'Secret KEY', 'Enter key')
+    def get_key(self):
+        secret_key, ok = self.show_input_dialog()
         if ok:
-            pass
+            return secret_key
+
+    def show_input_dialog(self):
+        return QtWidgets.QInputDialog.getText(self, 'Secret KEY', 'ENTER KEY')
 
     def authentication(self):
         login = self.login_input.text()
         password = self.password_input.text()
-        vk_tools.vk_auth(login=login, password=password)
-        # self.show_input_key_dialog()
+        self.vk_auth(login=login, password=password, auth_handler=self.auth_handler)
         self.clear_input_fields()
-       
+   
     def clear_input_fields(self):
         self.login_input.clear()
         self.password_input.clear()
-
 
 
 def main():
--- vk_tools.py
+++ vk_tools.py
@@ -1,21 +1,13 @@
 import vk_api
 
+class VkTools:
 
-def auth_handler():
-    key = input("Enter authentication code:")
-    remember_device = False
-    return key, remember_device
+    def vk_auth(self, login, password, auth_handler):
+        vk_session = vk_api.VkApi(login=login, password=password, auth_handler=auth_handler)
+        vk_session.auth()
+        vk = vk_session.get_api()
 
-
-def vk_auth(login, password):
-    vk_session = vk_api.VkApi(login=login, password=password, auth_handler=auth_handler)
-    vk_session.auth()
-    vk = vk_session.get_api()
-    response = vk.wall.get(count=1)
-    print(response)
-
-
-if __name__ == '__main__':
-    login = input()
-    password = input()
-    vk_auth(login, password)
+    def auth_handler(self):
+        key = self.get_key()
+        remember_device = False
+        return key, remember_device
